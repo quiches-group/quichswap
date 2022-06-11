366c6a31efa7f6f80be6e63a1019524d34c01754
Maxence Mottard <maxence.mottard@worldline.com>
2022-06-11 08:56:11 +0200

    setup tailwind css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import Quichtify from "quichtify"
 import 'quichtify/dist/style.css';
+import './index.css';
 
 import App from "./App.vue";
 import router from "./router";
