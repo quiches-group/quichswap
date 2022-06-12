cfec2dc3afff4d65c784626854b6a4ef3233f4a9
Zakarya Tolba <zakarya.tolba@ynov.com>
2022-06-12 21:54:14 +0200

    Added minting view & interface components for minting QCH tokens

--- src/views/Mint/MintView.vue
+++ src/views/Mint/MintView.vue
@@ -1,21 +1,59 @@
 <template>
-  <connect-button>
-    <button @click="mint">Mint</button>
-  </connect-button>
+  <div class="p-5 flex flex-col items-center">
+    <q-card class="p-10 bg-[#2b2d2e] outline-1">
+      <div class="flex flex-row">
+        <div class="flex-none">Your balance:</div>
+        <span class="flex-1" />
+        <div class="flex-none">{{ walletBalance }} QCH</div>
+      </div>
+      <div class="mt-5 flex flex-row">
+        <q-input v-model="state.amountInput" background-color="white" placeholder="Your amount" class="text-black w-full"></q-input>
+      </div>
+      <connect-button class="mt-5">
+        <q-button class="mt-5" @click="mint">Mint</q-button>
+      </connect-button>
+    </q-card>
+  </div>
 </template>
 
 <script setup>
-import { computed } from 'vue';
+import { computed, reactive, onMounted, watch } from 'vue';
 import { storeToRefs } from 'pinia';
 import { useWalletStore } from '../../stores/wallet.store';
 import ConnectButton from '../../components/ConnectButton.vue';
-import { SampleTokenContract } from '../../utils/contracts';
-import { toWei } from '../../utils/ethers';
+import { QCHTokenContract } from '../../utils/contracts';
+import { fromWei, toWei } from '../../utils/ethers';
 
-const { provider, wallet } = storeToRefs(useWalletStore());
-const stackingContract = computed(() => SampleTokenContract(provider.value));
+const { wallet, walletIsConnected, provider } = storeToRefs(useWalletStore());
+const mintingContract = computed(() => QCHTokenContract(provider.value));
+
+const state = reactive({
+  walletBalance: 0,
+  amountInput: null,
+});
+
+const walletBalance = computed(() => Number(fromWei(state.walletBalance)));
+const mintContract = computed(() => mintingContract.value.mintContract(provider.value));
+
+const fetchData = async () => {
+  if (!walletIsConnected.value) {
+    state.walletBalance = 0;
+
+    return;
+  }
+  state.walletBalance = await mintContract.value.balanceOf(wallet.value);
+};
 
 const mint = async () => {
-  await stackingContract.value.mint(wallet.value, toWei('500'));
+  await mintingContract.value.mint(wallet.value, toWei(state.amountInput.toString()));
+  await fetchData();
 };
+
+onMounted(() => {
+  fetchData();
+});
+
+watch(provider, () => {
+  fetchData();
+});
 </script>
