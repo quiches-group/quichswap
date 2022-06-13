5f16f9c908ce4261cd50a8ffe1985d03cb258074
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2022-06-13 23:45:05 +0200

    chore: setup colors in tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#42B883',
+        secondary: '#242526',
+        tertiary: '#2b2d2e',
+      },
+    },
   },
   plugins: [],
 };
