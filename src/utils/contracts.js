9845988575e6bd280c52d6b4c09f1829c3479020
Maxence Mottard <maxence.mottard@worldline.com>
2022-06-15 00:30:11 +0200

    change contract addresses

--- src/utils/contracts.js
+++ src/utils/contracts.js
@@ -8,13 +8,13 @@ import stackingMetadata from '../assets/abis/QuichswapStacking.json';
 import liquidityProvidingMetadata from '../assets/abis/QuichswapLiquidityProviding.json';
 
 const addresses = {
-  SampleTokenContract: '0x4aBb3a9f2A4eCf3360e4931d8F3805BC3eDF5070',
-  QCHTokenContract: '0x09744bc4cFA71EbDd57B5245F64f52bbcc919aC4',
-  STQCH_LPTokenContract: '0xBB490ae885CD2E5f07BC8e9881C41a16288696AC',
-  ST_StackingContract: '0x97e40D62060c0Ad64460A67B4ecDB30280AD2949',
-  QCH_StackingContract: '0x1BCf919f4c71279B50a86A91aCF424523d585d76',
-  STQCH_LiquidityProvidingContract: '0xa936e567B4023C81EB416063436Eb85A1e1a1925',
-  STQCHLP_StackingContract: '0xcF3ce0Ea19ce721a8D87146613C4A266C5Bd1634',
+  SampleTokenContract: '0x81D9a788e1BD1424E3a0D5A93C42624129D90738',
+  QCHTokenContract: '0xF3042Bc258CC1243eE5F5D25cD9e2AE377f1D49a',
+  STQCH_LPTokenContract: '0xed1d3AdEA5eb6807976F850F4cb9DD38C8b30814',
+  ST_StackingContract: '0xD9675c89cA350b2BFCc6920464f567a839794f5c',
+  QCH_StackingContract: '0xFDdaba1736f4A3c09A1D35Dd76AD080f58525E53',
+  STQCH_LiquidityProvidingContract: '0x31622E30216FB02C96FC9698898725ea4e138306',
+  STQCHLP_StackingContract: '0xeB6DE7027f6A76fF6cbDe89ab335b60c80739bc2',
 };
 
 export const SampleTokenContract = (provider) => new ethers.Contract(addresses.SampleTokenContract, sampleTokenMetadata.abi, provider);
