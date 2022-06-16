93f94eb38f2641fd922da69bee07364899c4a078
Guillaume Chx <guillaume.chateauroux@gmail.com>
2022-06-16 17:55:56 +0200

    Update: Made Graphs into components
    Todo:
    Create Tables
    Create Sub Pages with the created Components
    Make a fake Algorithm for Data

---
+++ src/views/Analytics/compoments/GraphCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-card class="w-full bg-neutral-800 border-neutral-700 border">
+    <div class="h-16 flex justify-between items-center">
+      <h2 class="p-5 text-xl font-light">{{ title }}</h2>
+      <q-tabs v-if="tabs.length" class="p-5 w-auto" :selected-index="selectedTab" @select-index="$emit('update:selectedTab', $event)">
+        <q-tab v-for="tab in tabs" :key="tab.value" class="text-white" :tab-index="tab.value">{{ tab.text }}</q-tab>
+      </q-tabs>
+    </div>
+    <q-separator class="mb-4 border-neutral-700" />
+    <slot />
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: 'GraphCard',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    tabs: {
+      type: Array,
+      default: () => [],
+    },
+    selectedTab: {
+      type: String,
+      default: null,
+    },
+  },
+  emits: ['update:selectedTab'],
+};
+</script>
+
+<style scoped></style>
