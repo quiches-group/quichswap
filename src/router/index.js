d7bf799b5dd7749b35182c1f00f562633199ff2c
Guillaume Chx <guillaume.chateauroux@gmail.com>
2022-06-21 00:41:05 +0200

    Added: Homepage

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,11 @@ import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: "/",
-    //   name: "home",
-    //   component: HomeView,
-    // },
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/Home/HomeView.vue'),
+    },
     {
       path: '/stack',
       name: 'Stack',
---
+++ src/views/Home/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="home" class="fixed bg-cover bg-center top h-screen w-screen flex flex-col justify-center items-center text-center">
+    <h1 class="text-4xl lg:text-6xl text-white">DeFi For The Next Quiche</h1>
+    <h2 class="my-5 text-md lg:text-2xl text-neutral-400">Swap assets instantly, globally, inexpensively, make money, automatically</h2>
+    <q-button color="#f40087" @click="$router.push({ name: 'Stack' })">Start Staking</q-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+};
+</script>
+
+<style scoped>
+#home {
+  margin: 0 !important;
+}
+
+#home:before {
+  content: ' ';
+  display: block;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0.3;
+  background-image: url('/src/assets/home-page-image.jpg');
+  background-repeat: no-repeat;
+  background-position: 50% 0;
+  background-size: cover;
+  z-index: -1;
+}
+</style>
