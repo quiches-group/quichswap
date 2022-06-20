7113b53181cb24cd5c5d0fc5e3a03005f8410192
Maxence Mottard <maxence.mottard@worldline.com>
2022-06-20 10:51:04 +0200

    change wallet

--- src/utils/contracts.js
+++ src/utils/contracts.js
@@ -8,13 +8,13 @@ import stackingMetadata from '../assets/abis/QuichswapStacking.json';
 import liquidityProvidingMetadata from '../assets/abis/QuichswapLiquidityProviding.json';
 
 const addresses = {
-  SampleTokenContract: '0x81D9a788e1BD1424E3a0D5A93C42624129D90738',
-  QCHTokenContract: '0xF3042Bc258CC1243eE5F5D25cD9e2AE377f1D49a',
-  STQCH_LPTokenContract: '0xed1d3AdEA5eb6807976F850F4cb9DD38C8b30814',
-  ST_StackingContract: '0xD9675c89cA350b2BFCc6920464f567a839794f5c',
-  QCH_StackingContract: '0xFDdaba1736f4A3c09A1D35Dd76AD080f58525E53',
-  STQCH_LiquidityProvidingContract: '0x31622E30216FB02C96FC9698898725ea4e138306',
-  STQCHLP_StackingContract: '0xeB6DE7027f6A76fF6cbDe89ab335b60c80739bc2',
+  SampleTokenContract: '0xe3b559e14C94eE45B9e4EaD6a58704DDab09e381',
+  QCHTokenContract: '0x55b16CCD19A5E8117F24b7572076d6F090363a88',
+  STQCH_LPTokenContract: '0x59fF9AaAD4d5512572cDe60Bf11165782553eA68',
+  ST_StackingContract: '0xF599AE5067F763B22b398d525596C38cCF33e009',
+  QCH_StackingContract: '0x300Fb6642D7073E90375A39139eB4a5B9B01A751',
+  STQCH_LiquidityProvidingContract: '0x86f52Ae564Fb5BD72Cb98e99c2f4419CE0cdBDD2',
+  STQCHLP_StackingContract: '0x5b642d57F782663e0a9781aFCD010D18f2a397B0',
 };
 
 export const SampleTokenContract = (provider) => new ethers.Contract(addresses.SampleTokenContract, sampleTokenMetadata.abi, provider);
--- src/utils/ethereum.js
+++ src/utils/ethereum.js
@@ -7,18 +7,37 @@ export const defaultProvider = !ethereum ? null : new ethers.providers.Web3Provi
 
 export const isEthereumInjected = !!ethereum;
 
-export const network = {
-  chainId: '0x13881',
-  chainName: 'Polygon Testnet',
-  nativeCurrency: { name: 'Matic', symbol: 'MATIC', decimals: 18 },
-  rpcUrls: ['https://matic-mumbai.chainstacklabs.com'],
-  blockExplorerUrls: ['https://mumbai.polygonscan.com/'],
-  iconUrls: [],
+export const networks = {
+  mumbai: {
+    chainId: '0x13881',
+    chainName: 'Polygon Testnet',
+    nativeCurrency: { name: 'Matic', symbol: 'MATIC', decimals: 18 },
+    rpcUrls: ['https://matic-mumbai.chainstacklabs.com'],
+    blockExplorerUrls: ['https://mumbai.polygonscan.com/'],
+    iconUrls: [],
+  },
+  ropsten: {
+    chainId: '0x3',
+    chainName: 'Ropsten',
+    nativeCurrency: { name: 'ETH', symbol: 'ETH', decimals: 18 },
+    rpcUrls: ['https://ropsten.infura.io/v3/7d114e1f288c42fbb534dd746703c4b7'],
+    blockExplorerUrls: ['https://ropsten.etherscan.io'],
+    iconUrls: [],
+  },
 };
 
+export const network = networks.ropsten;
+
 export const switchNetwork = async () => {
-  await ethereum.request({
-    method: 'wallet_addEthereumChain',
-    params: [network],
-  });
+  try {
+    await ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: network.chainId }],
+    });
+  } catch {
+    await ethereum.request({
+      method: 'wallet_addEthereumChain',
+      params: [network],
+    });
+  }
 };
