0940820346bf3f9ccbe1909bbb046d4d3906f668
Zakarya Tolba <zakarya.tolba@ynov.com>
2022-06-21 09:00:40 +0200

    Design fixes on swap & mint

--- src/views/Swap/SwapView.vue
+++ src/views/Swap/SwapView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="p-5 flex flex-col items-center">
     <h1 class="mt-10 text-4xl font-medium text-center">Swap</h1>
-    <p class="text-lg font-light text-center text-gray-200">Trade tokens in an instant</p>
+    <p class="text-lg font-light text-center text-gray-200">Trade tokens easily</p>
     <q-card class="flex flex-col gap-3 p-10 bg-tertiary mt-5">
       <div class="text-gray-500 text-base">Swap from:</div>
       <div class="flex flex-1 items-center p-2 rounded-lg bg-secondary">
